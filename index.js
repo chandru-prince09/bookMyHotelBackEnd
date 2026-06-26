84fec8a807e48ee4f80282c35a4ab61cd1caa0b9
chandru092008 <chandrusenthilkumar0903@gmail.com>
2026-06-26 15:33:03 +0530

    refactor: update wildcard route path to *splat in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hotelrouter = require("./routers/hotelrouter");
 const app = express();
 app.use(express.json());
 app.use("/api/v1/hotels", hotelrouter);
-app.all("*", (req, res) => {
+app.all("*splat", (req, res) => {
     res.status(404).json({
         status: "fail",
         message: ` the url ${req.originalUrl} not found`
